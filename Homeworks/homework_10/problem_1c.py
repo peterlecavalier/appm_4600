277d4f530b390a71a29651bf9d934d482342d4bb
Peter LeCavalier <pele6150@colorado.edu>
2022-11-01 22:30:11 -0600

    Started Homework 10

---
+++ Homeworks/homework_10/problem_1c.py
@@ -0,0 +1,34 @@
+import sys
+import os
+sys.path.insert(1, os.path.abspath('.\..\..'))
+from mypkg.Quadrature import MyQuad
+
+from scipy.integrate import quad
+
+##### Problem 1c #####
+# Define function
+f = lambda s: 1 / (1+s**2)
+
+# Initialize our integration techniques
+q = MyQuad(f, -5, 5)
+
+# Run Composite Trapezoidal
+comp_trap_result = q.composite_trap(n=1291)
+# Run Composite Simpson's
+comp_simp_result = q.composite_simp(n=108)
+# Run SciPy built-in quadrature
+scipy_result, scipy_err_est, info = quad(f, -5, 5, full_output=1)
+
+# Compute errors
+comp_trap_err = abs(comp_trap_result - scipy_result)
+comp_simp_err = abs(comp_simp_result - scipy_result)
+
+# Print the results of our integration techniques
+print(f"Composite Trapezoidal={comp_trap_result}, with error={comp_trap_err}")
+print(f"Composite Simpson's={comp_simp_result}, with error={comp_simp_err}")
+
+# Print number of function evaluations for SciPy built-in quadrature
+print(f"SciPy quad with 10^-6 tolerance evaluated f(s) {info['neval']} times.")
+scipy_result, scipy_err_est, info = quad(f, -5, 5, epsabs=1e-4, full_output=1)
+print(f"SciPy quad with 10^-4 tolerance evaluated f(s) {info['neval']} times.")
+##### END #####
---
+++ mypkg/Quadrature.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+class MyQuad():
+    def __init__(self,f=None,a=None,b=None, n=None):
+        self.f = f
+        self.a = a
+        self.b = b
+        self.n = n
+
+    def composite_trap(self,f=None,a=None,b=None,n=None):
+        # Verify that needed vars are inputted somewhere
+        if f is None:
+            if self.f is None:
+                raise ValueError("Please input f")
+            else:
+                f = self.f
+        if a is None:
+            if self.a is None:
+                raise ValueError("Please input a")
+            else:
+                a = self.a
+        if b is None:
+            if self.b is None:
+                raise ValueError("Please input b")
+            else:
+                b = self.b
+        if n is None:
+            if self.n is None:
+                raise ValueError("Please input n")
+            else:
+                n = self.n
+        
+        # Validate endpoints
+        if b < a:
+            raise ValueError("b must be greater than a")
+        elif b == a:
+            return 0
+        
+        h = (b - a)/n
+
+        xj = np.linspace(a, b, n+1)
+        fxj = f(xj)
+
+        return (h/2)*(fxj[0] + 2*np.sum(fxj[1:n]) + fxj[-1])
+
+    def composite_simp(self,f=None,a=None,b=None,n=None):
+        # Verify that needed vars are inputted somewhere
+        if f is None:
+            if self.f is None:
+                raise ValueError("Please input f")
+            else:
+                f = self.f
+        if a is None:
+            if self.a is None:
+                raise ValueError("Please input a")
+            else:
+                a = self.a
+        if b is None:
+            if self.b is None:
+                raise ValueError("Please input b")
+            else:
+                b = self.b
+        if n is None:
+            if self.n is None:
+                raise ValueError("Please input n")
+            else:
+                n = self.n
+        
+        # Validate endpoints
+        if b < a:
+            raise ValueError("b must be greater than a")
+        elif b == a:
+            return 0
+        
+        # Validate that n is even
+        if n % 2 != 0:
+            raise ValueError("n must be even")
+                
+        h = (b - a)/n
+
+        xj = np.linspace(a, b, n+1)
+        fxj = f(xj)
+
+        return (h/3)*(fxj[0] + 2*np.sum(fxj[2:n:2]) + 4* np.sum(fxj[1:n:2]) + fxj[-1])
+        
